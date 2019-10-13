703c9582519d25d71fd93df3b18d87c556626257
Nayla <naylasalzmann@gmail.com>
2019-10-13 10:27:19 -0400

    fix en layout

--- resources/views/layoutPdc.blade.php
+++ resources/views/layoutPdc.blade.php
@@ -18,34 +18,43 @@
     <title>@yield('title', 'Panel de control')</title>
 </head>
 <body>
-      @yield('content')
 
-       <ul id="slide-out" class="sidenav sidenav-fixed">
-          <li><div class="user-view">
-            <div class="background">
-              <img src="">
-            </div>
-            <a href="#user"><img class="circle" src=""></a>
-            <a href="#name"><span class="grey-text name">Admin</span></a>
-            <a href="#email"><span class="grey-text email">admin@gmail.com</span></a>
-          </div></li>
-          <li><a href="/pdc/reservas"><i class="material-icons">cloud</i>Reservas</a></li>
-          <li><a href="#!">Confirmaciones</a></li>
-          <li><div class="divider"></div></li>
-          <li><a class="subheader">Cancelaciones</a></li>
-          <li><a class="waves-effect" href="/pdc/salidas">Salidas</a></li>
-          <li><a class="waves-effect" href="/pdc/consultas">Consultas</a></li>
-          <li><a class="waves-effect" href="/pdc/guias">Guías</a></li>
-          <li><a class="waves-effect" href="/pdc/localidades">Localidades y zonas</a></li>
-          <li><a class="dropdown-trigger" href="/pdc/localidades" data-target="dropdown1">Otros<i class="material-icons right">arrow_drop_down</i></a></li>
-          <ul id='dropdown1' class='dropdown-content'>
-            <li><a href="/pdc/tiposSalida">Tipos de salida</a></li>
-            <li><a href="#!">Títulos</a></li>
-            <li><a href="/pdc/condiciones">Condiciones para reservar</a></li>
+  <div class="container">
+      <div class="row">
+        <div class="col s3">
+          
+         <ul id="slide-out" class="sidenav sidenav-fixed">
+            <li><div class="user-view">
+              <div class="background">
+                <img src="">
+              </div>
+              <a href="#user"><img class="circle" src=""></a>
+              <a href="#name"><span class="grey-text name">Admin</span></a>
+              <a href="#email"><span class="grey-text email">admin@gmail.com</span></a>
+            </div></li>
+            <li><a href="/pdc/reservas"><i class="material-icons">cloud</i>Reservas</a></li>
+            <li><a href="#!">Confirmaciones</a></li>
+            <li><div class="divider"></div></li>
+            <li><a class="subheader">Cancelaciones</a></li>
+            <li><a class="waves-effect" href="/pdc/salidas">Salidas</a></li>
+            <li><a class="waves-effect" href="/pdc/consultas">Consultas</a></li>
+            <li><a class="waves-effect" href="/pdc/guias">Guías</a></li>
+            <li><a class="waves-effect" href="/pdc/localidades">Localidades y zonas</a></li>
+            <li><a class="dropdown-trigger" href="/pdc/localidades" data-target="dropdown1">Otros<i class="material-icons right">arrow_drop_down</i></a></li>
+            <ul id='dropdown1' class='dropdown-content'>
+              <li><a href="/pdc/tiposSalida">Tipos de salida</a></li>
+              <li><a href="/pdc/titulos">Títulos</a></li>
+              <li><a href="/pdc/condiciones">Condiciones para reservar</a></li>
+            </ul>
           </ul>
-        </ul>
-      <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-     
+        </div>
+        <div class="col s9">
+          @yield('content')
+       
+        </div>
+      </div>
+   </div> 
+        <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>
 
      <!--JavaScript at end of body for optimized loading-->
       <script src="{{ asset('js/materialize.js') }}"></script>
