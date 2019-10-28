daf5266ea37863e06cf6473687f7ca23765abcdc
Nayla <naylasalzmann@gmail.com>
2019-10-27 22:41:26 -0400

    mejoras para mostrar fechas

--- app/Fecha.php
+++ app/Fecha.php
@@ -3,6 +3,8 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+use Carbon\Factory;
 
 class Fecha extends Model
 {
@@ -15,4 +17,48 @@ class Fecha extends Model
     	return $this->belongsTo(Salida::class);
 
     }
+
+    public function reservas()
+    {
+
+    	return $this->hasMany(Reserva::class);	
+
+    }
+
+    public function confirmaciones()
+    {
+
+        return $this->hasManyThrough(Confirmacion::class, Reserva::class);
+
+    }
+
+    public function getFormattedInicio() {
+
+    	$martinDateFactory = new Factory([
+    		'locale' => 'es_ES',
+    		'timezone' => 'America/Cordoba',
+		]);
+
+		//$dt = $this->inicio->toDayDateTimeString(); 
+		$dt = Carbon::parse($this->inicio);
+
+		$toDisplay = $martinDateFactory->make($dt)->isoFormat('lll');
+    	
+		return $toDisplay;
+    	//return Carbon::parse($this->inicio)->format('d-m-Y');
+	}
+
+	public function getDaysDuration() {
+		$inicio = Carbon::parse($this->inicio);
+		$fin = Carbon::parse($this->fin);
+		
+		return $inicio->diffInDays($fin);	
+	}
+
+	public function getHoursDuration() {
+		$inicio = Carbon::parse($this->inicio);
+		$fin = Carbon::parse($this->fin);
+		
+		return $inicio->diffInHours($fin);
+	}
 }
