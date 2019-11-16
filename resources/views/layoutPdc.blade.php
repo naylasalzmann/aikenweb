f72701cc77abd0d24af02aa0804a583175aa6866
Nayla <naylasalzmann@gmail.com>
2019-11-16 08:46:16 -0500

    creo login y middleware

--- resources/views/layoutPdc.blade.php
+++ resources/views/layoutPdc.blade.php
@@ -10,7 +10,6 @@
   <!--Import materialize.css-->
   <link type="text/css" rel="stylesheet" href="{{asset('css/materialize.css')}}"  media="screen,projection"/>
 
-   
 
   <!--TODO: cambiar el siguiente estilo a su propio file -->
   <style type="text/css">
@@ -29,13 +28,19 @@
       text-overflow: ellipsis;
     }
 
-
     tr:hover { 
       background: 'light-blue';  
     } 
 
     td a { 
       display: block; 
+    }
+
+    .mousescroll {
+      overflow: hidden !important;
+    }
+    .mousescroll:hover {
+      overflow-y: scroll !important;
     }
 
   </style>
@@ -51,17 +56,28 @@
 
   <div class="container">
       <div class="row">
-        <div class="col s3">
-          
-         <ul id="slide-out" class="sidenav sidenav-fixed">
-            <li><div class="user-view">
-              <div class="background">
+        <div class="col s3 ">
+         <ul id="slide-out" class="sidenav sidenav-fixed mousescroll">
+            <li>
+              <div class="user-view">
+              <div class="background teal lighten-1">
                 <img src="">
               </div>
               <a href="#user"><img class="circle" src=""></a>
-              <a href="#name"><span class="grey-text name">Admin</span></a>
-              <a href="#email"><span class="grey-text email">admin@gmail.com</span></a>
-            </div></li>
+              <div class="divider"></div>
+              <a href="#name"><span class="white-text name">{{ Auth::user()->name }}</span></a>
+              <a href="#email"><span class="white-text email">{{ Auth::user()->email }}</span></a>
+              <a class="white-text email" href="{{ route('logout') }}" 
+                  onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
+                  {{ __('Logout') }}
+              </a>
+              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                  @csrf
+              </form>
+              
+            </div>
+          </li>
             <li><a href="/pdc/dashboard">Dashboard</a></li>
             <li><a href="/pdc/consultas">Consultas</a></li>
             <li><a href="/pdc/reservas">Reservas</a></li>
@@ -112,4 +128,6 @@
       </script>
       @yield('scripts')
 </body>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -10,54 +10,63 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/', 'PagesController@index');
 
 Route::get('/{id}', 'PagesController@show');
 
-Route::get('/pdc/dashboard', 'DashboardController@index');
-
 Route::view('/checkout/thanks', 'thanks');
-
-Route::resource('/pdc/tiposSalida', 'TiposSalidaController');
-
-Route::resource('/pdc/condiciones', 'CondicionesController');
-
-Route::resource('/pdc/titulos', 'TitulosController');
-
-Route::resource('/pdc/guias', 'GuiasController');
-
-Route::resource('/pdc/localidades', 'LocalidadesController');
-
-Route::resource('/pdc/zonas', 'ZonasController');
-
-Route::resource('/pdc/salidas', 'SalidasController');
-
-Route::post('/salidas/{salida}/fechas', 'SalidaFechasController@store');
-
-Route::patch('/fechas/{fecha}', 'SalidaFechasController@update');
-
-Route::get('/pdc/fechas', 'SalidaFechasController@index');
 
 Route::get('/checkout/{id}', 'ReservasController@create');
 
 Route::post('/pdc/reservas', 'ReservasController@store'); //ver 
 
-Route::resource('/pdc/reservas', 'ReservasController')->only([  //ver
-    'index', 'show', 'edit', 'update', 'destroy'
-]);
+Route::group(['middleware' => 'auth'], function()
+{
+	Route::get('/pdc/dashboard', 'DashboardController@index');
+	
+	Route::resource('/pdc/tiposSalida', 'TiposSalidaController');
 
-Route::patch('/reservas/estado/{reserva}', 'ReservaEstadosController@update');
+	Route::resource('/pdc/condiciones', 'CondicionesController');
 
-Route::resource('/pdc/confirmaciones', 'ConfirmacionesController');
+	Route::resource('/pdc/titulos', 'TitulosController');
 
-Route::resource('/pdc/cancelaciones', 'CancelacionesController');
+	Route::resource('/pdc/guias', 'GuiasController');
 
-Route::resource('/pdc/cobros', 'CobrosController');
+	Route::resource('/pdc/localidades', 'LocalidadesController');
 
-Route::resource('/pdc/aventureros', 'AventurerosController');
+	Route::resource('/pdc/zonas', 'ZonasController');
 
-Route::resource('/pdc/consultas', 'ConsultasController');
+	Route::resource('/pdc/salidas', 'SalidasController');
+
+	Route::post('/salidas/{salida}/fechas', 'SalidaFechasController@store');
+
+	Route::patch('/fechas/{fecha}', 'SalidaFechasController@update');
+
+	Route::get('/pdc/fechas', 'SalidaFechasController@index');
+
+
+	Route::resource('/pdc/reservas', 'ReservasController')->only([  //ver
+	    'index', 'show', 'edit', 'update', 'destroy'
+	]);
+
+	Route::patch('/reservas/estado/{reserva}', 'ReservaEstadosController@update');
+
+	Route::resource('/pdc/confirmaciones', 'ConfirmacionesController');
+
+	Route::resource('/pdc/cancelaciones', 'CancelacionesController');
+
+	Route::resource('/pdc/cobros', 'CobrosController');
+
+	Route::resource('/pdc/aventureros', 'AventurerosController');
+
+	Route::resource('/pdc/consultas', 'ConsultasController');
+});
+
+
 
 
 
